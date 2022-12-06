073be24a91efe25f2ccd40d1935af7060ef165ef
dzexid <dzexid@icloud.com>
2022-12-06 16:44:10 +0200

    add secret key in token

--- app/vendors/helpers/token.py
+++ app/vendors/helpers/token.py
@@ -13,13 +13,13 @@ def check_user_token(user, token):
 	token_dict = eval(base64.b64decode(token))
 	if token_raw := token_dict.get('token', False):
 		token_parts = token_raw.split(':')
-		return user.id == int(token_parts[0]) and not eval(token_parts[-1])
+		return user.id == int(token_parts[0]) and token_parts[-2] == cfg('SECRET_KEY') and not eval(token_parts[-1])
 	return False
 
 def get_user_uid_token(user):
 	uid = str({'uid': str(user.id)}).encode('utf-8')
 	base64_uid = base64.b64encode(uid).decode('utf-8')
-	token_raw = f'{str(user.id)}:{str(datetime.timestamp(datetime.now()))}:{str(user.is_activated)}'
+	token_raw = f'{str(user.id)}:{str(datetime.timestamp(datetime.now()))}:{cfg('SECRET_KEY')}:{str(user.is_activated)}'
 	token = str({'token': token_raw}).encode('utf-8')
 	base64_token = base64.b64encode(token).decode('utf-8')
 	return {'uid': base64_uid, 'token':base64_token}
